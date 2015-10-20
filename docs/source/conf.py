4c75bb8cc21601cad7dfc8dfc443753fbac9fb41
Kyle Clemens <jkc.clemens@gmail.com>
2015-10-20 15:59:15 -0400

    Updated: This is dumb

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,7 @@ source_suffix = ['.rst', '.md']
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = 'index.rst'
 
 # General information about the project.
 project = 'khttp'
