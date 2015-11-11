0cae5224c03665bfd1687a22dca404cc1d0bff48
Kyle Clemens <jkc.clemens@gmail.com>
2015-11-10 20:41:53 -0500

    Added: Parameters tests

--- src/test/kotlin/khttp/structures/parameters/ParametersSpec.kt
+++ src/test/kotlin/khttp/structures/parameters/ParametersSpec.kt
@@ -7,6 +7,7 @@ package khttp.structures.parameters
 
 import khttp.MavenSpek
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
@@ -26,11 +27,27 @@ class ParametersSpec : MavenSpek() {
             val params = Parameters(map)
             on("inspecting the object") {
                 val size = params.size
+                val containsTest = "test" in params
+                val containsJest = "jest" in params
+                val containsTestValue = params.containsValue("value")
+                val containsJestValue = params.containsValue("lalue")
                 val test = params["test"]
                 val jest = params["jest"]
                 val toString = params.toString()
                 it("should have two items") {
                     assertEquals(2, size)
+                }
+                it("should have a test mapping") {
+                    assertTrue(containsTest)
+                }
+                it("should have a jest mapping") {
+                    assertTrue(containsJest)
+                }
+                it("should have a value value") {
+                    assertTrue(containsTestValue)
+                }
+                it("should have a lalue value") {
+                    assertTrue(containsJestValue)
                 }
                 it("should not have a null test mapping") {
                     assertNotNull(test)
@@ -46,6 +63,26 @@ class ParametersSpec : MavenSpek() {
                 }
                 it("should generate a parameter string on toString") {
                     assertEquals("test=value&jest=lalue", toString)
+                }
+            }
+            on("getting null") {
+                val result: Any? = params.getRaw(null)
+                it("should be null") {
+                    assertEquals(Unit, result)
+                    // KT-9963
+                    // assertNull(result)
+                }
+            }
+            on("checking for key null") {
+                val result = params.containsKeyRaw(null)
+                it("should be false") {
+                    assertFalse(result)
+                }
+            }
+            on("checking for value null") {
+                val result = params.containsValueRaw(null)
+                it("should be false") {
+                    assertFalse(result)
                 }
             }
         }
